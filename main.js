b9e335836db1ae1556d50adc3623864ffc22318e
jamespulley59 <jamespulley59@gmail.com>
2019-04-10 14:31:08 -0400

    almost

---
+++ main.js
@@ -0,0 +1,25 @@
+// get user input
+var filter = document.querySelector('#filter');
+
+
+// add event listener to listen to user input
+filter.addEventListener('keyup',filterNames)
+
+// filter out list depends on user input
+function filterNames() {
+    // get value of user input
+    var value = filter.value;
+    
+    // get all names in list
+    var names = document.querySelectorAll('li a');
+
+    //loop over names
+    for(var i=0; i<names.length; i++) {
+       // compare names
+       if(names[i].innerText.toUpperCase().includes(value.toUpperCase())) {
+            names[i].style.display = 'block'
+       } else {
+            names[i].style.display = 'none'
+       }
+    }
+}
